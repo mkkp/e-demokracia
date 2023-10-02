3e170d50f6fc3981ccc2f880845340ec5e52d037
Robert Csakany <csakany.robert@gmail.com>
2023-10-02 18:25:19 +0200

    Fixing deployer script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+mvn clean install -f application/karaf-features
+mvn clean install -f application/karaf-offline
+mvn clean install -f application/docker -Pcreate-docker-image-x86_64
+
+
 scp ./application/docker/target/edemokracia-application-*_docker_image_x86_64.tar robert.csakany@edemokracia.judo.technology:/tmp/edemokracia_image.tar
 
-scp ./update-image.sh robert.csakany@edemokracia.judo.technology:/tmp/update-image.sh
+#scp ./update-image.sh robert.csakany@edemokracia.judo.technology:/tmp/update-image.sh
 
 #ssh robert.cskaany@edemokracia.judo.technology /tmp/update-image.sh
